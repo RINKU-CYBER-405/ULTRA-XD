5b14fe67b59223fa6e1728474b72e382dac31fb2
Ayan ALve <115616439+RINKU-CYBER-405@users.noreply.github.com>
2023-04-08 22:44:23 +0600

    Update ULTRA.py

--- ULTRA.py
+++ ULTRA.py
@@ -1,17 +1,18 @@
-import os, platform
- 
-try:
-        import requests
-        os.system("git pull")
+from ULTRA import Rinku
+Rinku()
+
+
         
         
-except:
-        os.system('pip2 install requests')
-import requests
-bit = platform.architecture()[0]
-if bit == "64bit":
-        from ULTRA import register_device
-        register_device()
-elif bit == "32bit":
-        from ULTRA import register_device
-        register_device()
+        
+        
+
+
+
+
+
+        
+
+
+
+
